c33f9200bb5e8578069d1361e04dc7b335e1c272
min <dr94406@naver.com>
2023-10-23 23:34:29 +0900

    feat : 스웨거 연동 및 api 상세내역 작성

--- src/main/java/com/happy/play/controller/BoardController.java
+++ src/main/java/com/happy/play/controller/BoardController.java
@@ -2,14 +2,21 @@ package com.happy.play.controller;
 
 import com.happy.play.entity.Board;
 import com.happy.play.mapper.BoardMapper;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Slf4j
+@Tag(name = "board", description = "게시판 API")
+@RequestMapping("/api/v1/board")
 public class BoardController {
 
     private final BoardMapper boardMapper;
@@ -20,24 +27,28 @@ public class BoardController {
     }
 
     /* insert */
-    @PostMapping(path = "/board/save")
+    @PostMapping(path = "/save", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "게시글 저장", description = "게시글을 저장합니다.")
     public int save(@RequestBody Board board) {
         return boardMapper.save(board);
     }
 
     /* select */
-    @GetMapping(path = "/board/find")
+    @GetMapping(path = "/find", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "게시글 조회", description = "게시글을 조회합니다.")
     public List<Board> find() {
         return boardMapper.find();
     }
 
     /* update */
-    @PutMapping(path = "/board/update")
+    @PutMapping(path = "/update", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "게시글 수정", description = "게시글을 수정합니다.")
     public int update(@RequestBody Board board) {
         return boardMapper.update(board);
     }
     /* delete */
-    @DeleteMapping(path = "/board/delete/{id}")
+    @DeleteMapping(path = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "게시글 삭제", description = "게시글을 삭제합니다.")
     public int delete(@RequestBody Board board, @PathVariable int id) {
         return boardMapper.delete(id);
         }
--- src/main/java/com/happy/play/entity/Board.java
+++ src/main/java/com/happy/play/entity/Board.java
@@ -1,14 +1,23 @@
 package com.happy.play.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@Schema(description = "게시판 정보")
 public class Board {
 
+    @Schema(description = "게시글 아이디", defaultValue = "auto_increment")
     private int id;
+
+    @Schema(description = "게시글 작성자", example = "khm")
     private String author;
+
+    @Schema(description = "게시글 제목", example = "안녕하세요.")
     private String title;
+
+    @Schema(description = "게시글 내용", example = "게시판이 조용하네요.")
     private String content;
 }
